dd02c4d9e3634ce2e4d26becc333f93fe55e8ab5
simranaggarwal <simran.aggarwal@bluestone.com>
2023-10-24 13:26:29 +0530

    Renamed variable

--- src/com/examples/designpatterns/concurrencypatterns/threadlocal/user/UserContextManager.java
+++ src/com/examples/designpatterns/concurrencypatterns/threadlocal/user/UserContextManager.java
@@ -1,11 +1,22 @@
 package com.examples.designpatterns.concurrencypatterns.threadlocal.user;
 
+import java.util.function.Supplier;
+
 //User Thread Local
 public class UserContextManager {
 
-    private static final ThreadLocal<UserContext> userTheadLocal = ThreadLocal.withInitial(UserContext::new);
+    private static final ThreadLocal<UserContext> userThreadLocal =
+        ThreadLocal.withInitial(UserContext::new);
+        //ThreadLocal.withInitial(() -> new UserContext());
+        /*ThreadLocal.withInitial(new Supplier<UserContext>(){
+            @Override
+            public UserContext get() {
+                return new UserContext();
+            }
+        });*/
+
 
     public static UserContext getContext(){
-        return userTheadLocal.get();
+        return userThreadLocal.get();
     }
 }
